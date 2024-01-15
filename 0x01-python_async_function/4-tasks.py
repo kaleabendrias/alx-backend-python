9cbc578547332077a8d2c0f373e55c100306e254
kaleab <kaleabendrias1212@gmail.com>
2024-01-15 19:10:38 +0300

    Update task_wait_n to use asyncio.create_task()

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,5 +9,5 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """create wait_random"""
-    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    coroutines = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(coroutines)]
