f88d02ef1feb5f559003c876e08fafceb703c0ff
kaleab <kaleabendrias1212@gmail.com>
2024-01-15 18:57:06 +0300

    Add async functions and tasks

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import asyncio
+from typing import List
+from random import random
+
+
+async def wait_random(max_delay: int) -> float:
+    """async routine that takes in an integer argument (max_delay)"""
+    return random() * max_delay
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """spawn wait_random n times with the specified max_delay."""
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*coroutines)
+    return delays
