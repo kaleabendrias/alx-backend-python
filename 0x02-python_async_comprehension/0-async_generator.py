0bb3cba3d13d0c3390130997f56fdeeda710b21b
kaleab <kaleabendrias1212@gmail.com>
2024-01-16 14:18:26 +0300

    Update return type annotations in async_generator and async_comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """coroutine will loop 10 times, and yields random num"""
     for _ in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 """write a coroutine called async_comprehension"""
 
 
-import asyncio
+from typing import List
 import random
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> List[float]:
     """collect 10 random numbers using an async comprehensing"""
     return [i async for i in async_generator()]
