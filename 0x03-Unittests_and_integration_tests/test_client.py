56c829bdf8d07f477b3e87537aa24ea1a003f97d
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-03 10:58:10 +0300

    Refactor unit test for public_repos method in GithubOrgClient class

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -30,8 +30,11 @@ class TestGithubOrgClient(unittest.TestCase):
             result = test_instance._public_repos_url
             self.assertEqual(result, payload["repos_url"])
 
-    @patch('client.get_json', return_value=[{"name": "repo1"}, {"name": "repo2"}])
-    @patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock)
+    @patch('client.get_json', return_value=[{"name":
+                                            "repo1"},
+                                            {"name": "repo2"}])
+    @patch('client.GithubOrgClient._public_repos_url',
+           new_callable=PropertyMock)
     def test_public_repos(self, mock_public_repos_url, mock_get_json):
         """Unit test for GithubOrgClient.public_repos method."""
         mock_public_repos_url.return_value = "mocked_url"
